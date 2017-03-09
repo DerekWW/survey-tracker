1aae4fb0f2bf9d615777cfbf1081e8eb0cdbde00
DerekWW <derek.wierson@gmail.com>
2017-03-09 10:31:49 -0800

    work on nav and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view></router-view>
+    <foot></foot>
   </div>
 </template>
 
 <script>
+
+import navbar from './components/Navbar';
+import foot from './components/Footer';
+
+
 export default {
   name: 'app',
+  components: {
+    navbar,
+    foot,
+  },
 };
 </script>
 
@@ -16,8 +26,16 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+main {
+  flex: 1 0 auto;
 }
 </style>
